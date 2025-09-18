52910bd748b5cc5ff896ced5f2f05d6753b0d24a
cilginc <104676201+cilginc@users.noreply.github.com>
2025-09-18 23:43:52 +0300

    chore: changed terraform file paths

--- infra/production/security.tf
+++ infra/production/security.tf
@@ -3,8 +3,8 @@ resource "aws_security_group" "alb" {
   description = "Allow inbound HTTP from anywhere"
   vpc_id      = aws_vpc.main.id
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = var.host_port
+    to_port     = var.host_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }

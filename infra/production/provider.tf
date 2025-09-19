a822e52e33574260117c5d5c95a58013511aded6
cilginc <104676201+cilginc@users.noreply.github.com>
2025-09-19 21:44:19 +0300

    chore: added s3 bucket

--- infra/production/provider.tf
+++ infra/production/provider.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket-uniq-example"
+    key            = "terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-locks-example"
+    encrypt        = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
